1c76a50fb57090f1dec0b9c4e502a45d45728632
sooinice <sooin9674@naver.com>
2024-11-15 10:59:16 +0900

    Initial commit

---
+++ acha-web/src/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1 className="text-3xl font-bold text-blue-600">테스트</h1>);
