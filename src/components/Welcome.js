4a2da9e2cef2213312860777163a45c5b89ad3cb
sooinice <sooin9674@naver.com>
2024-12-24 17:09:54 +0900

    MOD : 랜딩 페이지 멘트수정

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,14 +3,12 @@ import { Link } from "react-router-dom";
 function Welcome() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
-      <h1 className="text-2xl font-bold mb-4">
-        반갑습니다! ACHA 서비스에 오신걸 환영합니다.
-      </h1>
+      <h1 className="text-6xl text-[74px] font-bold mb-48">Acha!</h1>
       <Link
         to="/login"
-        className="text-white bg-main-blue px-4 py-2 rounded-md hover:bg-blue-600"
+        className="text-white bg-main-blue px-12 py-4 rounded-md hover:bg-blue-600"
       >
-        시작하기
+        Start Right Now
       </Link>
     </div>
   );
