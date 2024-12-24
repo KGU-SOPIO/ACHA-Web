4dd0db7f4106e0edc15b266c80d4c023e85b3f90
sooinice <sooin9674@naver.com>
2024-12-24 09:34:31 +0900

    ADD : 오늘의 문구

--- src/home/Today.js
+++ src/home/Today.js
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 
+import { ReactComponent as QuoteLeft } from "../assets/“left.svg";
+import { ReactComponent as QuoteRight } from "../assets/“right.svg";
 import mockData from "../mocks/mocks.json";
+import quoteData from "../mocks/mock.json";
 
 function Today() {
   const [currentDate, setCurrentDate] = useState("");
+  const [randomQuote, setRandomQuote] = useState("");
 
   useEffect(() => {
     const date = new Date();
     const formattedDate = `(${date.getMonth() + 1}.${date.getDate()})`;
     setCurrentDate(formattedDate);
+
+    const randomIndex = Math.floor(Math.random() * quoteData.quotes.length);
+    setRandomQuote(quoteData.quotes[randomIndex]);
   }, []);
 
   return (
@@ -24,8 +31,8 @@ function Today() {
             {mockData.lectures.map((lecture, index) => {
               const backgroundColorClass =
                 lecture.dDay <= 3
-                  ? "bg-red-100 text-red-500"
-                  : "bg-blue-100 text-blue-700";
+                  ? "bg-red-100 text-red-600"
+                  : "bg-blue-100 text-blue-600";
 
               return (
                 <div
@@ -33,9 +40,11 @@ function Today() {
                   className="p-4 rounded-xl bg-white border border-gray flex justify-between w-full"
                 >
                   <div>
-                    <p className="text-xs">{lecture.professor} 교수님</p>
+                    <p className="text-xs text-gray-700">
+                      {lecture.professor} 교수님
+                    </p>
                     <p className="font-bold">{lecture.lectureName}</p>
-                    <p className="text-xs">{lecture.classroom}</p>
+                    <p className="text-xs text-gray-700">{lecture.classroom}</p>
                   </div>
                   <div
                     className={`w-auto px-4 py-2 flex items-center justify-center text-white font-bold text-xs rounded-full ${backgroundColorClass} self-center`}
@@ -53,6 +62,11 @@ function Today() {
           <span>오늘의 </span>
           <span className="font-bold">문구</span>
         </p>
+        <div className="mt-4 text-main-blue text-center relative">
+          <QuoteLeft className="flex absolute left-2 top-0 w-4 h-4 text-blue-500" />
+          <p className="p-6 text-xl font-bold mx-4">{randomQuote}</p>
+          <QuoteRight className="absolute right-2 bottom-0 w-4 h-4 text-blue-500" />
+        </div>
       </div>
     </div>
   );
