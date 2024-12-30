296f1353b90624a791994b8b6a450ea34226a2c4
sooinice <sooin9674@naver.com>
2024-12-30 18:51:39 +0900

    MOD : 상단바 수정

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,8 @@
 import { NavLink, useNavigate } from "react-router-dom";
 
 import { ReactComponent as Logo } from "./assets/sopio_logo.svg";
+import { ReactComponent as MyIcon } from "./assets/myIcon.svg";
+import { ReactComponent as NotificationsIcon } from "./assets/bellIcon.svg";
 import React from "react";
 
 const Nav = () => {
@@ -38,24 +40,20 @@ const Nav = () => {
               강좌
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to="/notifications"
-              className={({ isActive }) =>
-                isActive ? "text-main-blue " : "hover:text-main-blue"
-              }
-            >
-              알림
-            </NavLink>
-          </li>
         </ul>
-        <div>
-          <button
+        <div className="flex">
+          <NavLink to="/notifications" className="mr-[9px] ">
+            <NotificationsIcon className="bg-[rgba(240,246,255,1)] rounded-full" />
+          </NavLink>
+          <NavLink to="/myPage">
+            <MyIcon className="bg-[rgba(210,222,241,1)] rounded-full" />
+          </NavLink>
+          {/* <button
             onClick={handleLogout}
             className="text-red-500 bg-red-100 hover:bg-red-200 p-2 px-6 rounded-md"
           >
             로그아웃
-          </button>
+          </button> */}
         </div>
       </div>
     </nav>
