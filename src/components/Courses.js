f26e50d8aa4045ddffe71da4442fbb7723f82880
sooinice <sooin9674@naver.com>
2024-12-31 12:41:44 +0900

    MOD : 주차별 학습 활동 컴포넌트 분리

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,11 +1,7 @@
-import { ReactComponent as BookIcon } from "../assets/bookIcon.svg";
-import { ReactComponent as MediaIcon } from "../assets/mediaIcon.svg";
-import { ReactComponent as TaskIcon } from "../assets/task.svg";
+import WeeklyActivities from "../courses/WeeklyActivities";
 import mockData from "../mocks/courseMock.json";
 
 function Courses() {
-  const sortedWeeks = [...mockData[0].week].sort((a, b) => a.weekId - b.weekId);
-
   return (
     <div className="pt-[157px] px-[346px]">
       {/* Header Section */}
@@ -19,60 +15,11 @@ function Courses() {
         </button>
       </div>
       {/* Weekly Activities Section */}
-      <div>
-        <div className="flex ml-[22px] mb-[12px]">
-          <p className="text-[14px]">
-            <span className="font-bold">주차 별 </span>
-            <span>학습 활동</span>
-          </p>
-          <BookIcon />
-        </div>
-        <div className="flex space-x-4 overflow-x-auto">
-          {sortedWeeks.map((week) => (
-            <div key={week.weekId} className="flex space-x-4">
-              {/* Media Section */}
-              {week.media.concat(week.task).map((item) => (
-                <div
-                  key={item.mediaId || item.taskId}
-                  className="flex-none w-[300px] px-[24px] py-[21px] border rounded-xl"
-                >
-                  <p className="mb-[12px] text-[14px]">
-                    <span className="font-bold">{week.weekId}주차 </span>
-                    <span> [{item.date}]</span>
-                  </p>
-
-                  <div className="flex text-[14px] items-center justify-start p-[14px] border rounded-md">
-                    {item.mediaId ? (
-                      <>
-                        <MediaIcon className="w-[24px] h-[24px]" />
-                        <a
-                          href={item.link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="ml-[1px]"
-                        >
-                          {item.mediaName}
-                        </a>
-                      </>
-                    ) : (
-                      <>
-                        <TaskIcon className="w-[24px] h-[24px]" />
-                        <a
-                          href={item.link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="ml-[1px]"
-                        >
-                          {item.taskName}
-                        </a>
-                      </>
-                    )}
-                  </div>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+      <div className="pb-[48px] border-b mb-[26px]">
+        <WeeklyActivities />
+      </div>
+      <div className="bg-[rgba(245,246,248,1)] w-[1228px] h-[360px] rounded-xl px-[240px] py-[34px]">
+        sia
       </div>
     </div>
   );
