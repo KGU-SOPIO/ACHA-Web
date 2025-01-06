4f324ae3286bc9434df43063691efcb991793f01
sooinice <sooin9674@naver.com>
2025-01-06 11:25:56 +0900

    ADD : 사용자 상태 관리

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,23 +1,50 @@
+import React, { useState } from "react";
+
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
-import React from "react";
 
 function SignUp() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    studentNumber: "",
+    department: "",
+    major: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo((prevInfo) => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+  };
   return (
     <div className="flex flex-col h-screen bg-white">
       <main className="flex-grow flex flex-col items-center justify-center w-full max-w-md mx-auto py-12">
         <h2 className="text-4xl font-bold p-2">ACHA</h2>
 
-        <form className="flex flex-col items-center justify-center bg-white rounded-md border border-gray-300 p-12 w-full">
+        <form
+          className="flex flex-col items-center justify-center bg-white rounded-md border border-gray-300 p-12 w-full"
+          onSubmit={handleSubmit}
+        >
           <div className="mb-4 w-full">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="studentId"
+              htmlFor="name"
             >
               이름
             </label>
             <input
               id="name"
+              name="name"
+              value={userInfo.name}
+              onChange={handleChange}
               type="text"
               placeholder="이름"
               className="appearance-none border rounded w-full py-2 px-3 text-gray-700"
@@ -26,12 +53,15 @@ function SignUp() {
           <div className="mb-4 w-full">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="studentId"
+              htmlFor="studentNumber"
             >
               학번
             </label>
             <input
-              id="studentId"
+              id="studentNumber"
+              name="studentNumber"
+              value={userInfo.studentNumber}
+              onChange={handleChange}
               type="text"
               placeholder="학번"
               className="appearance-none border rounded w-full py-2 px-3 text-gray-700"
@@ -40,12 +70,15 @@ function SignUp() {
           <div className="mb-4 w-full">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="majorId"
+              htmlFor="department"
             >
               학부
             </label>
             <input
-              id="majorId"
+              id="department"
+              name="department"
+              value={userInfo.department}
+              onChange={handleChange}
               type="text"
               placeholder="학부"
               className="appearance-none border rounded w-full py-2 px-3 text-gray-700"
@@ -60,6 +93,9 @@ function SignUp() {
             </label>
             <input
               id="major"
+              name="major"
+              value={userInfo.major}
+              onChange={handleChange}
               type="text"
               placeholder="전공"
               className="appearance-none border rounded w-full py-2 px-3 text-gray-700"
@@ -74,6 +110,9 @@ function SignUp() {
             </label>
             <input
               id="password"
+              name="password"
+              value={userInfo.password}
+              onChange={handleChange}
               type="password"
               placeholder="비밀번호"
               className="appearance-none border rounded w-full py-2 px-3 text-gray-700"
