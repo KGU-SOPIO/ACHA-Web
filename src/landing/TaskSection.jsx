417f1d00f1f185dd201ea64e2938773b320c1092
sooinice <sooin9674@naver.com>
2025-04-07 01:13:14 +0900

    refactor: 랜딩페이지 스타일 수정

--- src/landing/TaskSection.jsx
+++ src/landing/TaskSection.jsx
@@ -3,32 +3,43 @@ import phoneImg from "../landing/phone_screen.png";
 export default function TaskSection() {
   return (
     <div className="flex flex-col lg:flex-row items-center justify-center bg-gray-100 mt-16 mb-[100px] px-4 sm:px-8 lg:px-16">
-      <div className="w-full lg:w-1/3 flex justify-center">
+      {/* 이미지 영역 */}
+      <div className="w-full lg:w-1/3 flex justify-center lg:justify-start mb-[40px]">
         <img
           src={phoneImg}
           alt="앱 화면"
-          className="w-[450px] h-[900px] sm:w-[300px] sm:h-[600px] lg:mr-[100px] mb-6 lg:mb-0"
+          className={`
+            w-[250px] h-[500px]
+            sm:w-[300px] sm:h-[600px]
+            md:w-[350px] md:h-[700px]
+            lg:w-[393px] lg:h-[800px]
+             lg:mb-0 lg:mr-[100px]
+          `}
         />
       </div>
 
-      <div className="flex flex-col text-center lg:text-end max-w-[600px]">
+      {/* 텍스트 영역 */}
+      <div
+        className={`
+          flex flex-col
+          /* 작은 화면에서 중앙 정렬, 큰 화면에서 오른쪽 정렬 */
+          text-center lg:text-end
+          max-w-[600px]
+        `}
+      >
         <h2 className="text-main-blue font-pretendard text-[24px] sm:text-[30px] lg:text-[36px] font-extrabold leading-[32px] sm:leading-[40px] lg:leading-[46px]">
           쉽고 빠른 과제를 수행 ✍️
         </h2>
 
-        <p className="text-[#1E1E1E] font-pretendard text-[14px] sm:text-[16px] font-normal leading-[24px] sm:leading-[30px]">
+        <p className="text-[#1E1E1E] font-pretendard text-[14px] sm:text-[16px] font-normal leading-[24px] sm:leading-[30px] mt-2 sm:mt-4">
           <span>각 주차별 강의와 과제 목록을 한눈에 확인할 수 있으며, </span>
-          <span className="text-[#1E1E1E] font-pretendard font-semibold">
-            강의 시험, 과제 제출,
-          </span>
+          <span className="font-semibold">강의 시험, 과제 제출,</span>
           <br />
-          <span className="text-[#1E1E1E] font-pretendard font-semibold">
-            관련 자료 확인 등을 한 곳에서 관리할 수 있습니다.
-          </span>
+          <span className="font-semibold">관련 자료 확인 등을</span> 한 곳에서
+          관리할 수 있습니다.
           <br />
           <span>
-            특히, 과제와 관련된 PDF 파일, 링크, 추가 자료 등을 함께 정리하여
-            사용자가{" "}
+            특히, 과제와 관련된 PDF 파일, 링크, 추가 자료 등을 함께 정리하여{" "}
           </span>
           <br />
           <span>
@@ -36,7 +47,9 @@ export default function TaskSection() {
           </span>
         </p>
 
-        <div className="flex flex-wrap gap-2 mt-4 justify-center lg:justify-end pb-[100px]">
+        <div
+          className={`flex flex-wrap gap-2 mt-4 justify-center lg:justify-end pb-[100px]`}
+        >
           <p className="text-gray-700 text-sm sm:text-base px-4 py-2 rounded-full border-gray-300 border">
             강의 시청
           </p>
@@ -47,7 +60,7 @@ export default function TaskSection() {
             파일
           </p>
           <p className="text-gray-700 text-sm sm:text-base px-4 py-2 rounded-full border-gray-300 border">
-            PDF
+            URL
           </p>
         </div>
       </div>
