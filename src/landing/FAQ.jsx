05fad4e949ac168ba985a4a944492ae863d012c1
sooinice <sooin9674@naver.com>
2025-04-07 19:40:11 +0900

    refactor: faq 세션 style 수정

--- src/landing/FAQ.jsx
+++ src/landing/FAQ.jsx
@@ -32,22 +32,20 @@ function FAQ() {
         {faqData.map((item, index) => (
           <div
             key={index}
-            className="border-[2px] border-gray-250  rounded-3xl transition-all duration-300 ease-in-out"
+            className="border border-[#DBDBDB] rounded-3xl transition-all duration-300 ease-in-out"
           >
             <div
-              className="flex justify-between items-center cursor-pointer px-[16px] md:px-6 py-[25px] md:py-5 w-full"
+              className="flex justify-between items-center cursor-pointer px-[25px] md:px-6 py-[25px] md:py-5 w-full text-[#464343]"
               onClick={() => toggleAnswer(index)}
             >
-              <h3 className="text-lg md:text-xl lg:text-2xl font-semibold">
-                {item.question}
-              </h3>
+              <h3 className="text-[15px] font-bold">{item.question}</h3>
               <span className="ml-2 text-xl w-[30px] h-[30px]">
                 {openIndices[index] ? <UpArrow /> : <DownArrow />}
               </span>
             </div>
 
             {openIndices[index] && (
-              <div className="overflow-hidden px-4 md:px-6 pb-4 md:pb-6">
+              <div className="px-[25px] pb-[30px]">
                 <p className="text-base md:text-lg pt-2 md:pt-4">
                   {item.answer}
                 </p>
